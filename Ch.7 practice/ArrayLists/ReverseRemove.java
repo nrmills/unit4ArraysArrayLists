9ab868eed91b97cb52b6f6c8cc496bf8b3c215a7
Nicolas Mills <millspec98@yahoo.com>
2014-12-01 12:22:41 -0600

    Reverse Remove

---
+++ Ch.7 practice/ArrayLists/ReverseRemove.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ReverseRemove
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> = new ArrayList<String>
+        
+        for( int i = list.size() - 1; i >= 0; i-- )
+        {
+            if( match.equals( list.get( i )))
+            {
+                list.remove( i );
+            }
+        }
+    }
+}
